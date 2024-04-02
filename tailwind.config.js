3b313f10720e3d4b3df01151a1992693facdffac
Anny <annylamberti2006@gmail.com>
2024-04-01 23:28:02 -0300

    vommitzinho gostoso delisioso marcio volta com o css por favor

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       'orange': '#E28625',
       'green': '#006971',
       'dark-green': '#004140',
-      'off-white': 'rgba(255, 255, 255, .75)',
+      'offwhite': 'rgba(255, 255, 255, .75)',
       'white': '#fff',
       'offwhitebg': '#f2f2f2'
     },
